8419f0cc2e4a48f951e13cfbbcaba23e79e759f8
Scott Brady <sbrady845@gmail.com>
2023-08-28 13:13:05 -0700

    Uploading excersize 2 from https://app.codingrooms.com/management/assignments/364786/overview

---
+++ Lesson 1/Excersize 2 - Debugging Practice.py
@@ -0,0 +1,18 @@
+#Excersie 2 - Debugging Practices
+#Original Code
+#Fix the code below 👇
+(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+#Fixed code with comments
+#Fix the code below 👇
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+#2. Outer double quotes changed to single quotes
+print('String Concatenation is done with the "+" sign.')
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
