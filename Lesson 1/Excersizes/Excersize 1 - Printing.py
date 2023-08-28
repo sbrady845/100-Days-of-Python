9e5b0cf7be1aefb501569e6fb978cb94a03350ac
Scott Brady <sbrady845@gmail.com>
2023-08-28 13:32:16 -0700

    Updated excersize files

---
+++ Lesson 1/Excersizes/Excersize 1 - Printing.py
@@ -0,0 +1,5 @@
+Example code output
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+
--- Lesson 1/Excersizes/Excersize 2 - Debugging Practice.py
+++ Lesson 1/Excersizes/Excersize 2 - Debugging Practice.py
@@ -1,11 +1,12 @@
 #Excersie 2 - Debugging Practices
 #Original Code
 #Fix the code below 👇
-(Day 1 - String Manipulation")
+""" 
+print(Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
   print('e.g. print("Hello " + "world")')
 print(("New lines can be created with a backslash and n.")
-
+"""
 #Fixed code with comments
 #Fix the code below 👇
 #1. Missing double quotes before the word Day.
