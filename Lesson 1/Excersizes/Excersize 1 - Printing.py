d7399446235420f4acc1d0500f50aacbfdc14439
Scott Brady <sbrady845@gmail.com>
2023-08-28 13:34:00 -0700

    Updated excersize 1

--- Lesson 1/Excersizes/Excersize 1 - Printing.py
+++ Lesson 1/Excersizes/Excersize 1 - Printing.py
@@ -1,5 +1,12 @@
+"""
 Example code output
 Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')
+"""
+
+#Write your code below this line 👇
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
 
