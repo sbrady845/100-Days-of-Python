f8e2f0408cd5e5dc366768d196a6f35d375a4cd8
Scott Brady <sbrady845@gmail.com>
2023-08-28 14:21:42 -0700

    Excersize 3 solution

---
+++ Lesson 1/Excersizes/Excersize 3 - Input Function.py
@@ -0,0 +1,11 @@
+"""
+Example Input
+Scott
+Example Output
+5
+"""
+#Calculate length of string
+#1st part - takes input of "What is your name"
+#2nd part - calculates the length of input
+#3rd part - prints the calculated length of input
+print(len(input("What is your name? ")))
