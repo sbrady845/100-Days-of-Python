9f7d50c10be40e5e73f6c341f0dacb653928b776
Scott Brady <sbrady845@gmail.com>
2023-09-08 12:13:53 -0700

    Updating work on Day 2 (09/08/2023)

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Excersizes/Excersize 5 - Number Manipulation and F-Strings.py
@@ -0,0 +1,28 @@
+#Examples of printing a division with decimal point repeating
+print(8/3)
+print(round(8/3, 2))
+print(round(2.6666666, 2))
+
+#Without converting into interger
+print(8 // 3)
+
+#Using varibale for printing math equations
+result = 4/2
+result /= 2
+print(result)
+
+#User scores a point
+score = 0
+score +=1
+
+secondScore = 0
+secondScore -=1
+
+print(score)
+print(secondScore)
+
+#Using f-strings
+fScore = 0
+fHeight = 1.8
+fIsWinning = True
+print(f"your score is {fScore}, your heigh is {fHeight}, you are winning is {fIsWinning}")
